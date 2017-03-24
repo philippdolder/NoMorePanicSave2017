87cb9a6ea892ad72ea5ab9e041e3d321761be865
Philipp Dolder <philipp.dolder@gmail.com>
2017-03-24 11:36:14 +0100

    Copied implementation from VS 2015 (without logging)

---
+++ source/NoMorePanicSave/NoMorePanicSavePackage.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using PhilippDolder.NoMorePanicSave;
+using Process = System.Diagnostics.Process;
+
+namespace NoMorePanicSave
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(NoMorePanicSavePackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string)]
+    public sealed class NoMorePanicSavePackage : Package
+    {
+        private WindowsEventHooker.WinEventDelegate otherApplicationFocusedHandlerReference;
+        private WindowsEventHooker.WinEventDelegate currentInstanceFocusedHandlerReference;
+        private IntPtr otherApplicationFocusedHookHandle;
+        private IntPtr currentInstanceFocusedHookHandle;
+        private SolutionEvents solutionEvents;
+        private bool visualStudioClosing;
+        private bool enabled;
+
+        /// <summary>
+        /// NoMorePanicSavePackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "22f14c01-29ae-462b-b601-171f054ed2d1";
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var visualStudioProcess = Process.GetCurrentProcess();
+            this.otherApplicationFocusedHandlerReference = this.HandleOtherApplicationFocused;
+            this.currentInstanceFocusedHandlerReference = this.HandleCurrentInstanceFocused;
+            this.otherApplicationFocusedHookHandle = WindowsEventHooker.SetWinEventHook(3, 3, IntPtr.Zero, this.otherApplicationFocusedHandlerReference, 0, 0, SetWinEventHookFlags.WINEVENT_OUTOFCONTEXT | SetWinEventHookFlags.WINEVENT_SKIPOWNPROCESS);
+            this.currentInstanceFocusedHookHandle = WindowsEventHooker.SetWinEventHook(3, 3, IntPtr.Zero, this.currentInstanceFocusedHandlerReference, (uint)visualStudioProcess.Id, 0, SetWinEventHookFlags.WINEVENT_OUTOFCONTEXT);
+
+            try
+            {
+                var dte = (DTE)this.GetService(typeof(DTE));
+
+                this.solutionEvents = dte.Events.SolutionEvents;
+                this.solutionEvents.BeforeClosing += this.HandleBeforeClosingSolution;
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            WindowsEventHooker.UnhookWinEvent(this.currentInstanceFocusedHookHandle);
+            this.currentInstanceFocusedHandlerReference = null;
+
+            WindowsEventHooker.UnhookWinEvent(this.otherApplicationFocusedHookHandle);
+            this.otherApplicationFocusedHandlerReference = null;
+
+            base.Dispose(disposing);
+        }
+
+        private void HandleBeforeClosingSolution()
+        {
+            this.visualStudioClosing = true;
+        }
+
+        private void HandleCurrentInstanceFocused(IntPtr hWinEventHook, uint eventType,
+                IntPtr hWnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            this.enabled = true;
+        }
+
+        private void HandleOtherApplicationFocused(
+                IntPtr hWinEventHook, uint eventType,
+                IntPtr hWnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            if (this.enabled && !this.visualStudioClosing)
+            {
+                this.SaveAll();
+
+                this.enabled = false;
+            }
+        }
+
+        private void SaveAll()
+        {
+            try
+            {
+                DTE dte = (DTE)this.GetService(typeof(DTE));
+
+                dte.ExecuteCommand("File.SaveAll");
+
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+        }
+    }
+}
